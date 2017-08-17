aef5aea476c2ad80b34bb772b83f13383e9dd433
300794883 <300794883@PR-A315-09>
2017-08-17 12:55:08 -0400

    Added Public property, and let the timer method shows PickHighestCard Form

--- COMP123-S2017-FinalExam-300794883/SplashForm.cs
+++ COMP123-S2017-FinalExam-300794883/SplashForm.cs
@@ -12,7 +12,7 @@ using System.Windows.Forms;
  * Name: Zilang Liang
  * Date: August 17, 2017
  * Description: This is the Splash Form
- * Version: 0.1 - Created the Splash Form
+ * Version: 0.2 - Added Public property, and let the timer method shows PickHighestCard Form
  */
 
 namespace COMP123_S2017_FinalExam_300794883
@@ -20,11 +20,11 @@ namespace COMP123_S2017_FinalExam_300794883
     public partial class SplashForm : Form
     {
         //PUBLIC PROPERTIES
-        public PickHighestCardForm PckHighestCardForm
+        public PickHighestCardForm PickHighestCardForm
         {
             get
             {
-                return Program.
+                return Program.pickhighestcardForm;
             }
         }
 
@@ -36,7 +36,11 @@ namespace COMP123_S2017_FinalExam_300794883
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            
+            this.PickHighestCardForm.Show();
+
+            this.Hide();
+
+            Timer.Enabled = false;
         }
     }
 }
