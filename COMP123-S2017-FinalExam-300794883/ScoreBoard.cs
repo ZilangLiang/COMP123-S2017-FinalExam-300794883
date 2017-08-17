b843ca123ca691a535d4439eff4f7412749e7b73
300794883 <300794883@PR-A315-09>
2017-08-17 13:37:49 -0400

    ScoreBoard Class

---
+++ COMP123-S2017-FinalExam-300794883/ScoreBoard.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace COMP123_S2017_FinalExam_300794883
+{
+    /*
+ * Name: Zilang Liang
+ * Date: August 17, 2017
+ * Description: This is the ScoreBoard class
+ * Version: 0.2 - ScoreBoard Class
+ */
+    public class ScoreBoard
+    {
+        //Private instance variables
+        private TextBox _finalScoreTextBox;
+        private int _score;
+        private TextBox _scoreTextBox;
+        private int _time;
+        private TextBox _timeTextBox;
+
+        //Properties
+        public TextBox FinalScoreTextBox
+        {
+            get
+            {
+                return this._score;
+            }
+            set
+            {
+                double c = Convert.ToInt32(_score);
+                c = value;
+            }
+        }
+        public int Score
+        {
+            get
+            {
+                double b = Convert.ToInt32(ScoreTextBox.Text);
+                return b;
+            }
+            set
+            {
+                double a = Convert.ToInt32(ScoreTextBox.Text);
+                a = value;
+            }
+        }
+        public int Time
+        {
+            get
+            {
+                return this._time;
+            }
+            set
+            {
+                double a = Convert.ToInt32(TimeTextBox.Text);
+                a = this._time;
+            }
+        }
+        public TextBox TimeTextBox { get; set; }
+        public TextBox ScoreTextBox { get; set; }
+
+        public ScoreBoard(TextBox scoreTextBox, TextBox timeTextBox, TextBox finalScoreTextbox)
+        {
+            ScoreTextBox = scoreTextBox;
+            TimeTextBox = timeTextBox;
+            FinalScoreTextBox = finalScoreTextbox;
+        }
+        public static void UpdateTime()
+        {
+            double a = Convert.ToInt32(TimeTextBox.Text);
+            a -= 1;
+        }
+
+    }
+}
--- COMP123-S2017-FinalExam-300794883/SplashForm.Designer.cs
+++ COMP123-S2017-FinalExam-300794883/SplashForm.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.components = new System.ComponentModel.Container();
             this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Timer
@@ -38,6 +40,28 @@
             this.Timer.Interval = 3000;
             this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(131, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(340, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to my Final Exam";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(133, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Please wait for 3 seconds";
+            // 
             // SplashForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -45,6 +69,8 @@
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(600, 500);
             this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -52,11 +78,14 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SplashForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
